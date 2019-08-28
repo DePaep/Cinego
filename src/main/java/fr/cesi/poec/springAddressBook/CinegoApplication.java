23ba9003a7916823cf4679afbbd85b79c467bf8e
Yvan De paep <depaep@hotmail.fr>
2019-08-28 16:11:41 +0200

    first commit

---
+++ src/main/java/fr/cesi/poec/springAddressBook/CinegoApplication.java
@@ -0,0 +1,13 @@
+package fr.cesi.poec.springAddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinegoApplication.class, args);
+	}
+
+}
