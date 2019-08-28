847ac69bc88ff47b75edcbb3af14a5c9c952c5fb
lenovo <lenovo@DESKTOP-S9DC33E>
2019-08-28 16:44:57 +0200

    classes fait

--- src/main/java/fr/cesi/poec/Cinego/entities/Salles.java
+++ src/main/java/fr/cesi/poec/Cinego/entities/Salles.java
@@ -1,5 +1,27 @@
 package fr.cesi.poec.Cinego.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//JPA - DB Mapping
+@Entity(name="Salles")
+
 public class Salles {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	
+	
 }
