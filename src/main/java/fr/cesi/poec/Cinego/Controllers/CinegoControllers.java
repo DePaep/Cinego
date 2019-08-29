a450faf13fa4b46c2f5b20d69e10be5fbd2bb2e4
xkenzzo31 <lucas.suquet@gmail.com>
2019-08-29 03:23:18 +0200

    bddCrée dao user

--- src/main/java/fr/cesi/poec/Cinego/Controllers/CinegoControllers.java
+++ src/main/java/fr/cesi/poec/Cinego/Controllers/CinegoControllers.java
@@ -5,7 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.cesi.poec.Cinego.entities.Movies;
@@ -23,6 +28,8 @@ public class CinegoControllers
 	{
 		this.movieRepo = movieRepo;
 	}
+
+
 	@GetMapping("")
 	public ResponseEntity<List<Movies>> getAllMovies()
 	{
